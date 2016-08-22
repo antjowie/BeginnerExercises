60b779e01db1f3243990c356f083fe31bc829172
Angelo Rettob <angelo05rettob@gmail.com>
2016-08-22 04:00:56 +0200

    Rebuilded project graduation because it was a mess ile:   Graduation/Graduation/Console output.txt

--- Graduation/Graduation/Source.cpp
+++ Graduation/Graduation/Source.cpp
@@ -5,278 +5,375 @@
 #include <thread> 
 #include <fstream>
 
+//Defines a sleep timer
 #define sleeptime std::chrono::seconds(0)
 #define sleep std::this_thread::sleep_for (sleeptime)
 
-struct type {
-	int age;
-	std::string name;
-	std::string sex;
-	std::string color;
-	bool rmvb;
-	type *next;
-}*root, *adress, *female;
+//Enumerated values for names and color
+enum names {George, Leeroy, Frogsron, Bob, Zakje, Patat, Kebab, Auto, Nope, Lamp, Mac, KFC, Bal, Rondje, Kip, Comb, Schipje, RNG, Weeb, Dota, EUW, Poku};
+enum color {White, Black, Brown, Sprouts};
 
-int rmvbammount;
-int count;
-int malecount;
-int femalecount;
-int Mmalecount;
-int Mfemalecount;
+int bunnyAmmount = 0;
+int MbunnyAmmount = 0;
 int turn = 0;
-int random;
-std::ofstream output;
-std::string rmvb;
+int fertile = 0;
+int infested;
 
-void printbunny();
-void generatebunny(int phase);
-void spawnbunny();
-void gathervalue();
-void killbunny();
-void infestbunny();
-void overflowbunny(int total);
-void insertfile(std::string line, int eventkind, int ammount);
+int debugAmmount = 0;
+int temptemp = 0;
+int number = 1;
+
+std::string tempRMVB;
+
+//Structure which contains all the necessary propeties of a bunny
+struct Bunny {
+	int age = 0;
+	int name;
+	std::string color;
+	std::string	sex;
+	bool RMVB;
+	Bunny *next = nullptr;
+}*root = new Bunny, *adress, *female;
+
+//Pre-defined function list
+void generateBunny(int phase);
+void gatherValues(void);
+void spawnBunny(void);
+void contaminationBunny(void);
+void killBunny(void);
+void overpopulationBunny(void);
+void printBunny(void);
+void insertFile(std::string line, int phase);
+
 int main() {
-
-//Initialize 5 starting bunnys
-	std::remove("console output.txt");
 	srand(time(0));
-	root = new type;
-	root->next = 0;
-	for (int i = 0; i < 5; i++)
-		generatebunny(0);
-	printbunny();
-	do{
-		gathervalue();
-		spawnbunny();
-		infestbunny();
-		killbunny();
-		turn++;
-		if (malecount + femalecount >= 1000)
-			overflowbunny((malecount + femalecount) / 2);
-		std::cout << "\nTurn " << turn << std::endl;
-		insertfile("\nTurn ", 3, turn);
-		printbunny();
-		std::cout << "Turn " << turn << std::endl;
-		std::cout << "Amount of bunnies is " << malecount+ femalecount<< std::endl;
-		insertfile("Turn ", 3, turn);
-		insertfile("\nAmount of bunnies is ", 3, malecount + femalecount);
-	} while (malecount + femalecount != 0);
+	remove("Console output.txt");
 
-	std::cout << "\nHow sad, no bunnies to be seen." << std::endl;
-	insertfile("\nHow sad, no bunnies to be seen.", 3, 0);
+	//Initialises 5 starting bunnys
+	for (int i = 5; i > 0; i--)
+		generateBunny(0);
+
+	//Simulator loop
+	while (bunnyAmmount != 0) {
+		temptemp = bunnyAmmount;
+		debugAmmount = 0;
+		gatherValues();
+		if (fertile > 0)
+			spawnBunny();
+		if (infested > 0)
+			contaminationBunny();
+		killBunny();
+		if (bunnyAmmount >= 1000)
+			overpopulationBunny();
+		printBunny();
+		sleep;
+	}
+
+	//End section
+	std::cout << "\nAll the bunnies have died! The bunnies existed for " << turn << " Turns\n The end" << std::endl;
+	insertFile(" Turns\n The end", 3);
+	sleep;
 	return 0;
 }
 
-void printbunny() {
+void generateBunny(int phase) {
+	adress = root;
+
+	//Locates latest list
+	while (adress->next != nullptr)
+		adress = adress->next;
+
+	//Creates new list
+	adress->next = new Bunny;
+	adress = adress->next;
+
+	//Generate bunny age
+	adress->age = 0;
+
+	//Switch which enters a name
+	/*
+	switch (rand() % 22) {
+	case George:
+		adress->name = "George";
+		break;
+	case Leeroy:
+		adress->name = "Leeroy";
+		break;
+	case Frogsron:
+		adress->name = "Frogsron";
+		break;
+	case Bob:
+		adress->name = "Bob";
+		break;
+	case Zakje:
+		adress->name = "Zakje";
+		break;
+	case Patat:
+		adress->name = "Patat";
+		break;
+	case Kebab:
+		adress->name = "Kebab";
+		break;
+	case Auto:
+		adress->name = "Auto";
+		break;
+	case Nope:
+		adress->name = "Nope";
+		break;
+	case Lamp:
+		adress->name = "Lamp";
+		break;
+	case Mac:
+		adress->name = "Mac";
+		break;
+	case KFC:
+		adress->name = "KFC";
+		break;
+	case Bal:
+		adress->name = "Bal";
+		break;
+	case Rondje:
+		adress->name = "Rondje";
+		break;
+	case Kip:
+		adress->name = "Kip";
+		break;
+	case Comb:
+		adress->name = "Comb";
+		break;
+	case Schipje:
+		adress->name = "Schipje";
+		break;
+	case RNG:
+		adress->name = "RNG";
+		break;
+	case Weeb:
+		adress->name = "Weeb";
+		break;
+	case Dota:
+		adress->name = "Dota";
+		break;
+	case EUW:
+		adress->name = "EUW";
+		break;
+	case Poku:
+		adress->name = "Poku";
+		break;
+	}
+	*/
+	adress->name = number;
+	number++;
+	//Switch which enters a color
+	if (phase == 0)
+		switch (rand() % 4) {
+		case White:
+			adress->color = "white";
+			break;
+		case Black:
+			adress->color = "black";
+			break;
+		case Brown:
+			adress->color = "brown";
+			break;
+		case Sprouts:
+			adress->color = "sprouts";
+			break;
+		}
+	else
+		adress->color = female->color;
+
+	//Defies sex and RMVB
+	if (rand() % 2 == 0) {
+		adress->sex = "male";
+		MbunnyAmmount++;
+	}
+	else
+		adress->sex = "female";
+	adress->RMVB = (rand() % 100 >= 98) ? true : false;
+
+	//Prints event
+	if (adress->RMVB == true) {
+		std::cout << "Radioactive mutant vampire bunny " << adress->name << " was born!\n";
+		//insertFile("generatebunny rmvb true\n", 2);
+		insertFile(" was born! ", 0);
+		infested++;
+	
+	}
+	else{
+		std::cout << "Bunny " << adress->name << " was born!\n";
+		//insertFile("generatebunny rmvb false\n ", 2);
+		insertFile(" was born! ", 0);
+	}
+	sleep;
+	debugAmmount++;
+	bunnyAmmount++;
+}
+
+void gatherValues() {
+	turn++;
+	infested = 0;
+
+	//Checks the bunnies properties for fertile, necessary for function spawnBunny
 	adress = root->next;
-	if (adress != 0)
+	while (adress != nullptr) {
+		adress->age++;
+		if (adress->age == 3 && adress->sex == "female" && adress->RMVB==false) {
+			std::cout << "Bunny " << adress->name << " became fertile\n";
+			//insertFile(" became fertile\n", 0);
+			sleep;
+		}
+		if (adress->age == 3 && adress->sex == "male" && adress->RMVB == false) {
+			std::cout << "Bunny " << adress->name << " is ready to mate\n";
+			//insertFile(" is ready to mate\n", 0);
+			fertile++;
+			sleep;
+		}
+		if (adress->RMVB == true)
+			infested++;
+		adress = adress->next;
+	}
+}
+
+void spawnBunny() {
+	female = root;
+	while (female != nullptr) {
+		if (female->age >= 3 && female->sex == "female" && female->RMVB == false)
+			generateBunny(1);
+		female = female->next;
+	}
+}
+
+void contaminationBunny() {
+	adress = root;
+	if (adress != nullptr)
+		while (adress != nullptr) {
+			//Breaks loop if all the infested bunnies are done contaminating the others
+			if (infested == 0)
+				break;
+			if (adress->RMVB == false) {
+				adress->RMVB = true;
+				if (adress->sex == "male")
+					fertile--;
+				std::cout << "Bunny " << adress->name << " has been contaminated\n";
+				//insertFile(" has been contaminated\n", 0);
+				infested--;
+				sleep;
+			}
+			adress = adress->next;
+		}
+}
+
+void killBunny() {
+	adress = root;
+	if (adress != nullptr)
+		while (adress != nullptr) {
+			Bunny * linker = adress;
+			//Checks RMVB value because they die in 50 years
+			if (adress->age == 10 && adress->RMVB == false) {
+				std::cout << "Bunny " << adress->name << " died!\n";
+				//insertFile("killbunny rmvb false\n", 2);
+				insertFile(" died! ", 0);
+				adress = adress->next;
+				delete linker;
+				bunnyAmmount--;
+				debugAmmount--;
+				if (adress->sex == "male")
+					fertile--;
+				sleep;
+			}
+			else if (adress->age == 50 && adress->RMVB == true) {
+				std::cout << "Bunny " << adress->name << " died!\n";
+				//insertFile("killbunny rmvb true\n", 2);
+				insertFile(" died! ", 0);
+				adress = adress->next;
+				delete linker;
+				bunnyAmmount--;
+				debugAmmount--;
+				sleep;
+			}
+			else
+				adress = adress->next;
+		}
+}
+
+void overpopulationBunny() {
+	int tempbunnyAmmount = bunnyAmmount / 2;
+	adress = root;
+	std::cout << "A food shortage has occured!\n";
+	insertFile("A food shortage has occured!\n", 2);
+	sleep;
+	while (tempbunnyAmmount != 0) {
+		//Randomizer for killing bunnies
+		Bunny * linker = adress;
+		if (rand() % 2 == 0) {
+			std::cout << "Bunny " << adress->name << " died!\n";
+			insertFile(" died!\n", 0);
+			adress = adress->next;
+			delete linker;
+			tempbunnyAmmount--;
+			bunnyAmmount--;
+			debugAmmount--;
+			if (adress->sex == "male"&&adress->RMVB == false)
+				fertile--;
+			sleep;
+		}
+		else
+			adress = adress->next;
+		if (adress == nullptr)
+			adress = root->next;
+	}
+}
+
+void printBunny() {
+	std::cout << "\n---\n";
+	insertFile("\n---\n", 5);
+	adress = root;
+	if (adress != nullptr)
 		for (int i = 50; i >= 0; i--) {
 			while (adress != nullptr) {
 				if (adress->age == i) {
-					std::string val = (adress->rmvb == true) ? "Positive" : "Negative";
-					std::cout << "Age " << adress->age << "\t| Name " << adress->name << "\t| Sex " << adress->sex << "\t| Color " << adress->color << "\t| Radioactive mutant vampire = " << val << "\t|\n";
-					output.open("console output.txt", std::ios::app);
-					output << "Age " << adress->age << "\t| Name " << adress->name << "\t| Sex " << adress->sex << "\t| Color " << adress->color << "\t| Radioactive mutant vampire = " << val << "\t|\n";
-					output.close();
+					tempRMVB = (adress->RMVB == true) ? "positive" : "negative";
+					//std::cout << "Age " << adress->age << "\t| Name " << adress->name << "\t| Sex " << adress->sex << "\t| Color " << adress->color << "\t| RMVB " << tempRMVB << "\t| " << std::endl;
+					//insertFile("", 4);
 				}
 				adress = adress->next;
 			}
 			adress = root->next;
 		}
+	std::cout << std::endl;
 	sleep;
 	sleep;
 }
 
-void generatebunny(int phase) {
-	
-	//Locates latest list
-	adress = root;
-	if (adress != 0)
-		while (adress->next != nullptr)
-			adress = adress->next;
-	adress->next = new type;
-	adress = adress->next;
-	adress->next = 0;
-	adress->age = 0;
-	//Names
-	random = rand() % 10;
-	switch (random) {
-	case 0:
-		adress->name = "Gordon";
-		break;
-	case 1:
-		adress->name = "Brotato";
-		break;
-	case 2:
-		adress->name = "SprEagle";
-		break;
-	case 3:
-		adress->name = "JohnCena";
-		break;
-	case 4:
-		adress->name = "Arnold";
-		break;
-	case 5:
-		adress->name = "Olegna";
-		break;
-	case 6:
-		adress->name = "Lamp";
-		break;
-	case 7:
-		adress->name = "Dolfyn";
-		break;
-	case 8:
-		adress->name = "Temmie";
-		break;
-	case 9:
-		adress->name = "MiBigBut";
-		break;
-	}
-	rmvb = (adress->rmvb == true)? "Radioactive mutant vampire bunny ": "Bunny ";
-	std::cout << rmvb << adress->name << " was born!" << std::endl;
-	insertfile(" was born!\n", 2, 0);
-	sleep;
-	//Sex
-	random = rand() % 2;
-	switch (random)	{
-	case 0:
-		adress->sex = "Male";
-		break;
-	case 1:
-		adress->sex = "Female";
-		break;
-	}
-	//Color
-	if (phase == 0) {
-	random = rand() % 4;
-	switch (random) {
-	case 0:
-		adress->color = "white";
-		break;
-	case 1:
-		adress->color = "brown";
-		break;
-	case 2:
-		adress->color = "black";
-		break;
-	case 3:
-		adress->color = "spotted";
-		break;
-	}
-	}
-	else 
-		adress->color = female->color; 
-	//rmvb
-	random = rand() % 100 + 1;
-	if (random <= 2)
-		adress->rmvb = true;
-	else
-		adress->rmvb = false;
-}
-
-void spawnbunny() {
-	female = root;
-	adress = root;
-
-	if (adress != 0)
-		while (female->next != nullptr) {
-			female = female->next;
-			if (female->sex == "Female" && female->age >= 2 && female->rmvb == false)
-				generatebunny(1);
+void insertFile(std::string line, int phase) {
+	//phase 0 Bunny .name. "line"
+	//phase 1 RMVB bunny .name. "line"
+	//phase 2 "line"
+	//phase 3 \nAll the bunnies have died! The bunnies existed for ..turn.. "line"
+	//phase 4 printBunny
+	//phase 5 debug
+	std::ofstream insert("Console output.txt", std::ios::out | std::ios::app);
+	if (insert.is_open()) {
+		switch (phase) {
+		case 0:
+			insert << "Bunny " << adress->name << line << std::endl;
+			break;
+		case 1:
+			insert << "Radioactive mutant vampire bunny " << adress->name << line;
+			break;
+		case 2:
+			insert << line;
+			break;
+		case 3:
+			insert << "\nAll the bunnies have died! The bunnies existed for " << turn << line;
+			break;
+		case 4:
+			insert << "Age " << adress->age << "\t| Name " << adress->name << "\t| Sex " << adress->sex << "\t| Color " << adress->color << "\t| RMVB " << tempRMVB << "\t|" << std::endl;
+			break;
+		case 5:
+			insert << line << " action " << debugAmmount << " old val " << temptemp << " should be " << temptemp+debugAmmount << " real val " << bunnyAmmount << std::endl;
+			break; 
 		}
-}
-
-void gathervalue(){
-	count = 0;
-	rmvbammount = 0;
-	malecount = 0;
-	femalecount = 0;
-	adress = root->next;
-	if (adress != 0)
-		while (adress != nullptr) {
-			adress->age++;
-			if (adress->sex == "Male")
-				malecount++;
-			if (adress->sex == "Male" && adress->age >= 2)
-				Mmalecount++;
-			if (adress->sex == "Female")
-				femalecount++;
-			if (adress->sex == "Female" && adress->age >= 2)
-				Mfemalecount++;
-			if (adress->rmvb == true)
-				rmvbammount++;
-			adress = adress->next;
-		}
-}
-
-void killbunny() {
-	adress = root->next;
-	if (adress != 0)
-		while (adress != nullptr) {
-			if (adress->age == 10 && adress->rmvb == false ) {
-				std::cout << "Bunny " << adress->name << " died of age!" << std::endl;
-				adress = adress->next;
-				delete female->next;
-				female = adress;
-				sleep;
-			}
-			else if (adress->age == 50 && adress->rmvb == true) {
-				std::cout << "Bunny " << adress->name << " died of age!" << std::endl;
-				adress = adress->next;
-				delete female->next;
-				female = adress;
-				sleep;
-			}
-			adress = adress->next;
-		}
-}
-
-void infestbunny() {
-	adress = root->next;
-	if (adress != 0)
-		while (adress != nullptr && rmvbammount > 0) {
-			if (adress->rmvb == false) {
-				adress->rmvb = true;
-				std::cout << "Bunny " << adress->name << " became a radioactive mutant vampire bunny!" << std::endl;
-				insertfile("became a radioactive mutant vampire bunny!\n", 0, 0);
-				rmvbammount--;
-				sleep;
-			}
-			adress = adress->next;
-		}
-}
-
-void overflowbunny(int total) {
-	std::cout << "Oh no! There is an abundance of bunny which leeds to a shortage of food! " << total << " Bunnies are dying!";
-	insertfile("Oh no! There is an abundance of bunny which leeds to a shortage of food! ", 1, total);
-	sleep;
-	adress = root->next;
-	for (total; total >= 0; total--) {
-		random = rand() % 1;
-		while (adress != nullptr) {
-			female = adress;
-			if (random == 0) {
-				std::cout << "Bunny " << adress->name << " died of food shortage!" << std::endl;
-				insertfile(" died of food shortage!\n", 0, 0);
-				adress = adress->next;
-				delete female->next;
-				break;
-			}
-			adress = adress->next;
-		}
+		insert.close();
 	}
-}
-
-void insertfile(std::string line, int eventkind, int ammount) {
-	output.open("console output.txt", std::ios::out | std::ios::app);
-	if (eventkind == 0)
-		output << "Bunny" << adress->name << line;
-	else if (eventkind == 1)
-		output << line << ammount << "Bunnies are dying!\n";
-	else if (eventkind == 2)
-		output << rmvb << adress->name << line;
-	else if (eventkind == 3)
-		output << line;
-	output.close();
 }
