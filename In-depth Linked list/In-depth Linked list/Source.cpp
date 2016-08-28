72215e60071092de7c24fda0cd0fb32ca10d08d2
Angelo Rettob <angelo05rettob@gmail.com>
2016-08-28 15:20:43 +0200

    Added project In-depth Linked list which is actually a playlist program for music

--- In-depth Linked list/In-depth Linked list/Source.cpp
+++ In-depth Linked list/In-depth Linked list/Source.cpp
@@ -2,107 +2,130 @@
 #include <string>
 #include <sstream>
 
-struct Music {
+struct song {
 	int number;
+	std::string title = "HEAD";
+	song *next = nullptr;
+}*root, *list;
+
+void addSong(const std::string &title, int &total);
+void removeSong(const int &number, const std::string &title, int &total);
+void listSongs(const int &total);
+
+int main() {
+	int input;
+	int total = 0;
 	std::string title;
-	Music * next;
-};
-
-
-
-Music * head = new Music;
-Music * next;
-Music * destructor;
-std::string name;
-int number;
-int total = 0;
-int newnumber;
-
-void add(const std::string &title);
-void remove(const std::string &title = "nullptr", const int &number = 0);
-void list(int phase = 1);
-
-int	main() {
+	root = new song;
 	do {
-		std::cout << "Make a choice by index\n 0 - Exit\t1 - Add song\t2 - Remove song\t3 - list songs\n";
-		std::cin >> number;
-		std::cin.ignore();
-		//Main menu
-		switch (number) {
+		std::cout << "\nSelect by index...\n"
+			<< "1 - add song.\n"
+			<< "2 - remove song.\n"
+			<< "3 - list songs.\n"
+			<< "0 - quit program.\n";
+		std::cin >> input;
+		switch (input) {
 		case 0:
+			std::cout << "\nExiting program...\n";
 			break;
 		case 1:
-			std::cout << "Enter the name of the song" << std::endl;
-			std::getline(std::cin, name);
-			add(name);
+			std::cout << "\nEnter title...\n";
+			std::cin.ignore();
+			std::getline(std::cin, title);
+			addSong(title,total);
 			break;
 		case 2:
-			std::cout << "Make a choice by index\n 0 - Type name\t1 - Type index\n";
-			list();
-			std::cin >> number;
-			switch (number) {
-			case 0:
-				std::getline(std::cin, name);
-				remove(name);
-				break;
-			case 1:
-				std::cin >> number;
-				remove("",number);
-				break;
-			default:
-				std::cout << "Incorrect value!\n";
+			if (total > 0) {
+				std::cout << "\nSelect by index...\n"
+					<< "1 - remove song by title.\n"
+					<< "2 - remove song by number.\n";
+				std::cin >> input;
+				listSongs(total);
+				switch (input) {
+				case 1:
+					std::cout << "\nEnter title...\n";
+					std::cin.ignore();
+					std::getline(std::cin, title);
+					input = 0;
+					break;
+				case 2:
+					std::cout << "Enter number...\n";
+					std::cin >> input;
+					break;
+				default:
+					std::cout << "(ERROR) entered invalid value!\n";
+					break;
+				}
+				removeSong(input, title, total);
+				input = 3;
 			}
+			else
+				std::cout << "\nNo songs to be found...\n";
+			break;
 		case 3:
-			list();
+			listSongs(total);
 			break;
 		default:
-			std::cout << "Incorrect value!\n";
+			std::cout << "(ERROR) entered invalid value!\n";
+			break;
 		}
-	} while (number != 0);
+	} while (input != 0);
 	return 0;
 }
 
-void add(const std::string &title) {
-	list();
-	next = new Music;
-	next->title = title;
-	list(2);
+void addSong(const std::string &title, int &total) {
+	list = root;
+	while (list->next != nullptr)
+		list = list->next;
+	list->next = new song;
+	list = list->next;
+	list->title = title;
 	total++;
 }
 
-void remove(const std::string &title, const int &number) {
-	list();
-	//Initiates phase 3
-	if (title != "nullptr")
-		list(3);
-	else
-		list(4);
+void removeSong(const int & number, const std::string & title, int & total){
+	song * destructor;
+	destructor = root;
+	//runs remove title program
+	if (number == 0) {
+		while (destructor->title != title) {
+			list = destructor;
+			destructor = destructor->next;
+			if (destructor == nullptr) {
+				std::cout << "\n(ERROR) entered invalid title! try by index.\n";
+				return;
+			}
+		}
+	}
+	else 
+		while (destructor->number != number) {
+			list = destructor;
+			destructor = destructor->next;
+			if (destructor == nullptr) {
+				std::cout << "\n(ERROR) entered invalid number! try a lower number.\n";
+				return;
+			}
+		}
+	std::cout << "\nRemoved\n" << destructor->number << "\t|\t" << destructor->title << "\nSuccessfully!\n";
+	//runs remove middle node program if the song isn't the last one
+	if (destructor->next != nullptr)
+		list->next = list->next->next;
+	delete destructor;
 }
 
-void list(int phase) {
-	//phase 1 prints linked lists
-	//phase 2 insert number value during function add
-	//phase 3 remove by index
-	//phase 4 remove by string
-	next = head->next;
-	if (next != nullptr && phase == 1 || phase == 2)
-		for (int i = 1; i <= total; i++)
-			while (next != nullptr) {
-				if (next->number == i && phase == 1)
-					std::cout << next->number << " - " << next->title << std::endl;
-				else if (phase == 2){
-					if (i != next->number){
-						newnumber = i;
-						break;
-					}
-				}
-				else if (phase == 3 || phase == 4) {
-					destructor = next;
-					if (next->number == number && phase == 3 || next->title == name && phase == 4) {
-						delete destructor;
-						break;
-					}
-				}
-				next = next->next;
-			}
-}
+void listSongs(const int &total) {
+	if (total != 0) {
+		std::cout << std::endl;
+		int counter = 1;
+		list = root->next;
+		while (list != nullptr) {
+			list->number = counter;
+			std::cout << list->number << "\t|\t" << list->title << '\n';
+			counter++;
+			list = list->next;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "\nNo songs to be found...\n";
+	}
